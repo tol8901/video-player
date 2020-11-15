700db76abe935f00f281f9af1ff517f4c461cab3
Anatolii_Kravchenko <tol890@ukr.net>
2020-11-15 14:23:47 +0300

    progress bar - dynamic width

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const video = document.querySelector('video');
 const progressRange = document.querySelector('progress-range');
-const progressBar = document.querySelector('progress-bar');
+const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('volume-range');
@@ -31,7 +31,10 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
-
+// Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 // Volume Controls --------------------------- //
 
@@ -47,3 +50,5 @@ video.addEventListener('ended', showPlayIcon);
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
